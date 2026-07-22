edaf1b3cc430d37aa66f912d01d036a1da608591
Nivrutti Chavan <chavannivrutti130@gmail.com>
2026-07-22 06:56:28 +0530

    Hasing in set

---
+++ Hashing/set.py
@@ -0,0 +1,9 @@
+#user only to check item is in set or not
+online_user=set()
+online_user.add("Nivrutti")
+online_user.add("Nitin")
+online_user.add("Amar")
+online_user.add("Nitin")#set only get unique values(Nitin is repeated to it take only one)
+
+print(online_user)
+print("Nitin" in online_user)
